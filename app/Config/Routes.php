79623d7ca0871c82af3ca78364ca3dddbf8e0587
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-08-24 12:52:45 +0200

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,6 +9,6 @@ $routes->get('/', 'Home::index');
 
 
 $routes->get('login', 'AuthController::login');
-$routes->post('login', 'AuthController::login');
+$routes->post('login', 'AuthController::loginPost'); // si tu as un POST
 $routes->get('logout', 'AuthController::logout');
 $routes->get('dashboard', 'DashboardController::index', ['filter' => 'auth']);
