f8281082ec767b6f92bbf16bcca2dd157ee3907f
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-08-24 13:05:52 +0200

    go

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,3 +12,4 @@ $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::loginPost'); // si tu as un POST
 $routes->get('logout', 'AuthController::logout');
 $routes->get('dashboard', 'DashboardController::index', ['filter' => 'auth']);
+
