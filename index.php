ca69891c95f802d33fa85c2a7fbb1c3374cd4910
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-08-26 23:56:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -90,6 +90,18 @@ switch (ENVIRONMENT)
 		exit(1); // EXIT_ERROR
 }
 
+// Patch PHP 8.2 pour CodeIgniter 3
+if (PHP_VERSION_ID >= 80200) {
+    #[\AllowDynamicProperties]
+    class_alias('CI_Controller', '__CI_Controller');
+    #[\AllowDynamicProperties]
+    class_alias('CI_Model', '__CI_Model');
+    #[\AllowDynamicProperties]
+    class_alias('CI_Router', '__CI_Router');
+    #[\AllowDynamicProperties]
+    class_alias('CI_URI', '__CI_URI');
+}
+
 /*
  *---------------------------------------------------------------
  * SYSTEM DIRECTORY NAME
