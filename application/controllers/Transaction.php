38f6022ad803184b28427e26f7960c16553e88bf
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-08-29 19:53:15 +0200

    Update Transaction.php

--- application/controllers/Transaction.php
+++ application/controllers/Transaction.php
@@ -12,14 +12,14 @@ class Transaction extends BaseController {
         $this->load->library('session');
         $this->load->helper('url');
     }
-    
+
     public function index() {
-        $this->loadViews('transactions/list', []);
+        $this->loadViews('transactions/list',  $this->global, NULL, NULL);
     }
     public function sales() {
-        $this->loadViews('transactions/sales', []);
+        $this->loadViews('transactions/sales',  $this->global, NULL, NULL);
     }
     public function rentals() {
-        $this->loadViews('transactions/rentals', []);
+        $this->loadViews('transactions/rentals',  $this->global, NULL, NULL);
     }
 }
