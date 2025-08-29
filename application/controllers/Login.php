ea52be9324087bdf6a54875deb6c00a5f61bd02e
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-08-29 14:33:31 +0200

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -117,10 +117,7 @@ class Login extends CI_Controller {
                     if ($m->meta_key == 'fave_author_custom_picture') {
                         $avatar_url = $m->meta_value;
                     }
-                    
                 }
-
-
 
                 $this->session->set_userdata([
                     'logged_in'   => true,
@@ -132,7 +129,7 @@ class Login extends CI_Controller {
                     'isLoggedIn'  => TRUE,
                     'wp_url'      => null // Set if you store user URL
                 ]);
-              //  redirect('dashboard');
+                redirect('dashboard');
             }
         }
 
