4f329ee43f3d00585e8e52e6bf0c7f9941c94c64
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-08-29 18:16:05 +0200

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -793,7 +793,7 @@
                         // Example: $role = 'super_admin' | 'agency_admin' | 'agent'
                         // You should set $role in your controller/session logic
 
-                        if ($role === 'adminstrator') : ?>
+                        if ($role === 'administrator') : ?>
                             <li class="nav-item">
                                 <a class="nav-link menu-link" href="<?php echo base_url('dashboard'); ?>">
                                     <i class="ri-dashboard-2-line"></i> <span>Tableau de bord global</span>
