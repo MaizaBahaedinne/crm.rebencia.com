fe4a43e33b190ab24a9193c04111ede989e223a4
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-08-29 17:53:02 +0200

    Update Dashboard.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,6 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+
+
+require APPPATH . '/libraries/BaseController.php';
+
+
+
 class Dashboard extends BaseController {
     public function __construct() {
         parent::__construct();
