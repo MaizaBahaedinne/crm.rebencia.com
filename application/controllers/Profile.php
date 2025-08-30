e2be96a346e314b87a6e5141040db7b61104e28f
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-08-30 21:29:57 +0200

    go

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -4,6 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 require APPPATH . '/libraries/BaseController.php';
 
+/**
+ * @property User_model $user_model
+ */
 class Profile extends BaseController {
     
     public function __construct() {
@@ -18,11 +21,26 @@ class Profile extends BaseController {
     public function index() {
         $this->isLoggedIn();
        
-        $data['user'] = $this->user_model->get_wp_user($this->vendorId,'wp_Hrg8P_');
+        $wpUser = $this->user_model->get_wp_user($this->vendorId,'wp_Hrg8P_');
+        if($wpUser){
+            $meta = isset($wpUser->meta)? $wpUser->meta : [];
+            $data['user'] = [
+                'id' => $wpUser->ID,
+                'login' => $wpUser->user_login,
+                'email' => $wpUser->user_email,
+                'name' => $wpUser->display_name,
+                'roles' => $wpUser->roles ?? [],
+                'avatar' => base_url('assets/images/users/avatar-1.jpg'), // TODO: remonter avatar WP si disponible
+                'location' => $meta['billing_city'] ?? ($meta['city'] ?? ''),
+                'mobile' => $meta['mobile'] ?? ($meta['billing_phone'] ?? ''),
+                'joining_date' => isset($meta['user_registered']) ? date('d/m/Y', strtotime($meta['user_registered'])) : '',
+                'agence' => $meta['agency_name'] ?? ''
+            ];
+        } else {
+            $data['user'] = [];
+        }
 
-        print_r($data['user']);
-
-       // $this->loadViews('profile/index', $this->global, $data, NULL);
+        $this->loadViews('profile/index', $this->global, $data, NULL);
     }
    
 }
