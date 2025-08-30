d21513d57c9e8fa371805f1b22b7bbeee94ffa3a
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-08-30 12:11:27 +0200

    Update Profile.php

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -3,7 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 require APPPATH . '/libraries/BaseController.php';
+
 class Profile extends BaseController {
+    
     public function __construct() {
         parent::__construct();
         $this->load->model('agent_model');
