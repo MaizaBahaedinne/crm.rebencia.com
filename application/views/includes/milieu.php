92384f26efd74584f4a8041a2795f1422f77ae9d
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-09-05 09:44:08 +0200

    Create milieu.php

---
+++ application/views/includes/milieu.php
@@ -0,0 +1,38 @@
+<div class="main-content">
+    <div class="page-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-title-box d-sm-flex align-items-center justify-content-between bg-galaxy-transparent">
+                        <h4 class="mb-sm-0">Détail propriété</h4>
+                        <div class="page-title-right">
+                            <ol class="breadcrumb m-0">
+                                <li class="breadcrumb-item"><a href="<?php echo base_url('dashboard'); ?>">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Détail propriété</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-4">
+                <div class="card-body">
+                    ici le continue de la page 
+                </div>
+            </div>
+        </div>
+    </div>
+     <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <script>document.write(new Date().getFullYear())</script> © Velzon.
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="text-sm-end d-none d-sm-block">
+                                Design & Develop by Rebencia
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+</div>
