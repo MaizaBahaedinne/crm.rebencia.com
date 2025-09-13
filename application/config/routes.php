28eb5fd5f6cdd4d0aa6c503d71bac28b89b48f9b
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-09-13 18:35:08 +0200

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,7 @@ $route['agency/view/(:num)'] = 'Agency/view/$1';
 /*********** AGENTS (HOUZEZ - Consultation) *******************/
 $route['agents'] = 'Agent/index';
 $route['agents/test'] = 'Agent/test';
+$route['agents/test_view/(:num)'] = 'Agent/test_view/$1';
 $route['agents_simple/test'] = 'Agent_simple/test';
 $route['agents/performance'] = 'Agent/performance';
 $route['agents/view/(:num)'] = 'Agent/view/$1';
