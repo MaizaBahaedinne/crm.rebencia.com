7f541146342150841c77ad01159b43ea35bf8c6f
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-09-21 19:10:00 +0200

    Update Objective_model.php

--- application/models/Objective_model.php
+++ application/models/Objective_model.php
@@ -251,7 +251,7 @@ class Objective_model extends CI_Model
             // Récupérer les IDs des agents de l'agence
             $agents = $this->get_agents_by_agency($agency_id);
             echo(json_encode( $agents ));   
-            $agent_ids = array_column($agents, 'ID');
+            $agent_ids = array_column($agents, 'user_id');
             if (empty($agent_ids)) {
             $objectives = [];
             } else {
